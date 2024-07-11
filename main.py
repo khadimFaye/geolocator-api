c03cf2fba3c51d978b5f9318e0fc008019a4f780
Cheikhul Xadim Faye <97457513+khadimFaye@users.noreply.github.com>
2024-07-11 12:09:27 +0000

    main

--- main.py
+++ main.py
@@ -1,11 +1,26 @@
-from fastapi import FastApi
-from fastaapi.cors import CorsMiddleware
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+import uvicorn
 
-
-app = FastApi(__dirname__)
+#inizializza fastapi 
+app = FastAPI()
+#aggiungui corsmiddle ware per gestire meglio i blocchi dovuti ai cors durante le requets http
+app.add_middleware(
+  CORSMiddleware,
+    
+    allow_credentials=True,
+    allow_origins=['*'],
+    allow_methods=['*'],
+    allow_headers=['*']
+  )
+#test the firs route
 @app.get('/')
 async def sayhello():
   return {'greeting': 'hallo'}
+
+
+if __name__=='__main__':
+  uvicorn.run(app)
 
 
 
